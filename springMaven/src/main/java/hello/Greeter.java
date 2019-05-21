cb73c61ac4592ac4c3aacd6027b74dffc3d0775f
tyler <tylerkranig@gmail.com>
2019-05-21 17:39:36 -0500

    some testing through maven

--- springMaven/src/main/java/hello/Greeter.java
+++ springMaven/src/main/java/hello/Greeter.java
@@ -1,4 +1,4 @@
-package main.java.test;
+package hello;
 
 public class Greeter {
     public String sayHello() {
--- springMaven/src/main/java/hello/HelloWorld.java
+++ springMaven/src/main/java/hello/HelloWorld.java
@@ -1,7 +1,11 @@
-package main.java.test;
+package hello;
+
+import org.joda.time.LocalTime;
 
 public class HelloWorld {
     public static void main(String[] args) {
+        LocalTime date = new LocalTime();
+        System.out.println("The current time is " + date);
         Greeter greeter = new Greeter();
         System.out.println(greeter.sayHello());
     }
